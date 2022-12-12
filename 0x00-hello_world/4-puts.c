2144466c9f365ce646b3c58b89e3246e73ce708e
kdentu <kelvin.emmadentu@gmail.com>
2022-12-12 13:35:09 -0800

    Hello world task and betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#Include <stdio.h> 
+/**
+ * main - A program that prints puts funtion 
+ * Return: # (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
