69266269ef80d6575d066f183a41de1d2e13ce2e
kdentu <kelvin.emmadentu@gmail.com>
2022-12-19 21:31:14 -0800

    Pointers task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * reset_to_98-Resets the value of a variable back to 98.
+ * @n:Variable to be reset.
+ * Return:Void.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
+Task 1
+#include "main.h"
+
+/**
+ * swap_int-Swaps the values of 2 integers.
+ * @a:First argument to be swapped.
+ * @b:second argument to be swapped.
+ *
+ * Return:void.
+ */
